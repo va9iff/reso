20ef9032597bde36535fac1412b2c91a208275fe
va9iff <va9iff@gmail.com>
2023-05-04 01:58:19 +0400

    delete unnecessary menu panes

--- src/side-menu.js
+++ src/side-menu.js
@@ -14,9 +14,6 @@ class SideMenu extends VLitElement{
 			<button @click=${e=>this.sideBarActivate("anaMenyu")} ?active = ${this.active=="anaMenyu"} ?hidden=${window.V.sideBarMinimized}>Ana menyu</button>
 			<button @click=${e=>this.sideBarActivate("bugun")} ?active = ${this.active=="bugun"} ?hidden=${window.V.sideBarMinimized} active>Bugün</button>
 			<button @click=${e=>this.sideBarActivate("sifaris")} ?active = ${this.active=="sifaris"} ?hidden=${window.V.sideBarMinimized}>Sifariş</button>
-			<button @click=${e=>this.sideBarActivate("tarixce")} ?active = ${this.active=="tarixce"} ?hidden=${window.V.sideBarMinimized}>Tarixçə</button>
-			<button @click=${e=>this.sideBarActivate("yemekElavesi")} ?active = ${this.active=="yemekElavesi"} ?hidden=${window.V.sideBarMinimized}>Yemək əlavəsi</button>
-			<button @click=${e=>this.sideBarActivate("adminMenyu")} ?active = ${this.active=="adminMenyu"} ?hidden=${window.V.sideBarMinimized}>Admin menyu</button>
 			<button @click=${e=>{
 				this.sideBarActivate("")
 			}} ?hidden = ${!window.V.active || window.V.sideBarMinimized || true}>Bağla</button>
