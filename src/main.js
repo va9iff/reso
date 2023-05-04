33b84609bf6ab6d7cb078cdc94b563005db15be0
va9iff <va9iff@gmail.com>
2023-05-05 02:21:11 +0400

    add boring opening text

--- src/main.js
+++ src/main.js
@@ -10,7 +10,49 @@ import "./food-menu.js"
 import "./daily-calc.js"
 import "./add-order.js"
 let sideTabs = {
-	home: ()=>html`<h3>Xoş gəlmişsiniz</h3>`,
+	home: ()=>html`<div style="padding: 14px; font-size: 1.3rem">
+	Gündəlik qeydiyyatınızın ən rahat şəkildə aparılmasına köməklik edən və
+	avtomatik hesablamaları yerinə yetirə bilən elektron estoran menecment 
+	sisteminə xoş gəlmişsiniz. <br>
+	<br>
+	Sol tərəfdəki menyudan bir çox əməliyyatlar üçün interfeyslərə çıxış vardır.
+	<ul style="padding: 20px">
+		<br>
+		<li>
+			<b>Ana Menyu</b>: <br> 
+			Yeməklərinizi kateqoriyalara uyğun bölüşdürüb nəzər sala bilərsiniz.
+		</li>
+		<br>
+		<li>
+			<b>Bugün</b>: <br> 
+			Bugünlük bütün sifarişlərin çeklərini (hesablarını) görə bilərsiniz. 
+			Hər bir sifarişdə hansı yeməkdən neçə ədəd olduğunu, qiymətlərini 
+			və nəhayət bütün yeməklərin qiymətini, yəni sifariş məbləğini görə 
+			bilərsiniz. Ayrıca bütün sifarişlərin də cəmini buradan izləyə 
+			bilərsiniz. Heç bir hesablama əməliyyatı aparmağınıza ehtiyac 
+			yoxdur, proqram təminatı özü sizin bunu üçün edir.
+		</li>
+		<br>
+		<li>
+			<b>Sifariş</b>: <br>
+			Yeni bir sifariş qeydə almaq üçün bu interfeysdən istifadə edin. 
+			Sadəcə sayını daxil edib yeməyin üstünə klik etdikdə müvafiq 
+			yemək hesaba əlavə olunacaq. Aşağıda isə çekin şəkli hazırlanacaq. 
+			Müştəri hesabı tələb etdikdə birbaşa çap edib təhvil vermək 
+			mümkündür. Çekdə yeməklərin cəmi də avtomatik şəkildə hesablanır.
+		</li>
+		<br>
+	</ul>
+
+	<br>
+	<br>
+	<br>
+	Sistemə sahib olmaq 330₼ təşkil edir.<br>
+
+
+
+
+	</div>`,
 	[""]: ()=>html``,
 	anaMenyu: ()=>html`<food-menu></food-menu>`,
 	bugun: ()=>html`<daily-calc></daily-calc>`,
@@ -28,7 +70,7 @@ class VMain extends VLitElement{
 	}
 	constructor(){
 		super()
-		this.active = this.active = "sifaris"
+		this.active = this.active = "home"
 		this.sideBarMinimized = false
 	}
 	connectedCallback(){
